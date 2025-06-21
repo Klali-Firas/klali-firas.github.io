f05896452778fdb989d8f8d3f37c80a8b760c5c2
Klali-Firas <firasklali1234@gmail.com>
2025-06-21 15:01:11 +0100

    feat: update sidebar component with additional navigation buttons for Experience and Projects

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [ButtonModule],
+  imports: [ButtonModule, CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
