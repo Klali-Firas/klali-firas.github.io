fefa3999da2b629dd4808c38845cea510d277973
Klali-Firas <firasklali1234@gmail.com>
2025-06-21 13:39:15 +0100

    feat: implement sidebar component with toggle functionality and responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "./about/about.component";
+import { ButtonModule } from "primeng/button";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [AboutComponent],
+  imports: [AboutComponent, ButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'peronal-portfolio';
+  sidebarVisible = true;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+  get isSmallScreen(): boolean {
+    return window.innerWidth < 768;
+  }
 }
